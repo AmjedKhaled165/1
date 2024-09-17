021a962a6b8c73d235aeca2b6569fa25ea810e29
Amjed <122521459+AmjedKhaled165@users.noreply.github.com>
2024-09-17 22:45:27 +0300

    ass1.py

--- ass1.py
+++ ass1.py
@@ -45,3 +45,35 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('yolov8n.pt')  
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLOv8 Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
